b90daf1196aa804370774ab518c676477424f867
k0zmo <koozmoo@gmail.com>
2013-04-09 14:13:38 +0200

    added querying cameras info and settings theirs parameters

--- Logic/Jai/JaiNodeModule.cpp
+++ Logic/Jai/JaiNodeModule.cpp
@@ -3,6 +3,12 @@
 
 #if defined(HAVE_JAI)
 
+template<typename T> void setNodeValue(CAM_HANDLE hCamera, const char* nodeName, T value);
+template<> void setNodeValue<int64_t>(CAM_HANDLE hCamera, const char* nodeName, int64_t value);
+template<typename T> RangedValue<T> queryNodeRangedValue(CAM_HANDLE hCamera, const char* nodeName);
+template<> RangedValue<int64_t> queryNodeRangedValue<int64_t>(CAM_HANDLE hCamera, const char* nodeName);
+vector<tuple<int64_t, string>> queryNodeEnumValue(CAM_HANDLE hCamera, const char* nodeName);
+
 JaiNodeModule::JaiNodeModule()
 	: _factoryHandle(nullptr)
 {
@@ -56,9 +62,142 @@ bool JaiNodeModule::ensureInitialized()
 	return true;
 }
 
-std::string JaiNodeModule::moduleName() const
+string JaiNodeModule::moduleName() const
 {
 	return "jai";
+}
+
+vector<CameraInfo> JaiNodeModule::discoverCameras()
+{
+	vector<CameraInfo> list;
+	if(!ensureInitialized())
+		return list;
+
+	// In fact, cameras are already discovered, we just need to query for their names
+	for(auto iter = _camHandles.begin(), end = _camHandles.end(); iter != end; ++iter)
+	{
+		J_STATUS_TYPE error;
+		int8_t cameraId[J_CAMERA_ID_SIZE];
+		uint32_t size = uint32_t(sizeof(cameraId));
+
+		if((error = J_Factory_GetCameraIDByIndex(_factoryHandle, 
+			iter - _camHandles.begin(), cameraId, &size)) != J_ST_SUCCESS)
+		{
+			continue;
+		}
+
+		// Get iD name
+		CameraInfo cameraInfo;
+		cameraInfo.id = string(reinterpret_cast<char*>(cameraId));
+
+		int8_t buffer[J_CAMERA_INFO_SIZE];
+		auto queryCameraInfo = [&](int8_t cameraId[], 
+			J_CAMERA_INFO camInfo) -> string
+			{
+				uint32_t size = uint32_t(sizeof(buffer));
+				if((error = J_Factory_GetCameraInfo(_factoryHandle, cameraId,
+					camInfo, buffer, &size)) != J_ST_SUCCESS)
+				{
+					return string();
+				}
+				return string(reinterpret_cast<char*>(buffer));
+			};
+
+		string modelName = queryCameraInfo(cameraId, CAM_INFO_MODELNAME);
+		if(cameraInfo.id.find("INT=>FD") != string::npos)
+			modelName += " (Filter driver)";
+		else if(cameraInfo.id.find("INT=>SD") != string::npos)
+			modelName += " (Socket driver)";
+
+		cameraInfo.manufacturer = queryCameraInfo(cameraId, CAM_INFO_MANUFACTURER);
+		cameraInfo.interfaceId = queryCameraInfo(cameraId, CAM_INFO_INTERFACE_ID);
+		cameraInfo.ipAddress = queryCameraInfo(cameraId, CAM_INFO_IP);
+		cameraInfo.macAddress = queryCameraInfo(cameraId, CAM_INFO_MAC);
+		cameraInfo.serialNumber = queryCameraInfo(cameraId, CAM_INFO_SERIALNUMBER);
+		cameraInfo.userName = queryCameraInfo(cameraId, CAM_INFO_USERNAME);		
+		list.emplace_back(std::move(cameraInfo));
+	}
+
+	return list;
+}
+
+
+CameraSettings JaiNodeModule::cameraSettings(int index)
+{
+	CameraSettings set;
+	if(!ensureInitialized())
+		return set;
+
+	if(size_t(index) >= (int)_camHandles.size())
+		throw JaiException(J_ST_INVALID_ID, "Invalid camera Id");
+
+	try
+	{
+		auto& camHandle = _camHandles[index];
+		bool tmpHandle = false;
+		
+		// Open camera if necessary
+		if(!camHandle)
+		{
+			camHandle = openCamera(index);
+			tmpHandle = true;
+		}
+
+		set.offsetX = queryNodeRangedValue<int64_t>(camHandle, NODE_NAME_OFFSETX);
+		set.offsetY = queryNodeRangedValue<int64_t>(camHandle, NODE_NAME_OFFSETY);
+		set.width = queryNodeRangedValue<int64_t>(camHandle, NODE_NAME_WIDTH);
+		set.height = queryNodeRangedValue<int64_t>(camHandle, NODE_NAME_HEIGHT);
+		set.gain = queryNodeRangedValue<int64_t>(camHandle, NODE_NAME_GAIN);
+		set.pixelFormat = queryNodeValue<int64_t>(camHandle, NODE_NAME_PIXELFORMAT);
+		set.pixelFormats = queryNodeEnumValue(camHandle, NODE_NAME_PIXELFORMAT);
+
+		if(tmpHandle)
+			closeCamera(camHandle);
+
+		return set;
+	}
+	catch(JaiException& ex)
+	{
+		return set;
+	}
+}
+
+bool JaiNodeModule::setCameraSettings(int index, const CameraSettings& settings)
+{
+	if(!ensureInitialized())
+		return false;
+
+	if(size_t(index) >= (int)_camHandles.size())
+		throw JaiException(J_ST_INVALID_ID, "Invalid camera Id");
+
+	try
+	{
+		auto& camHandle = _camHandles[index];
+		bool tmpHandle = false;
+		
+		// Open camera if necessary
+		if(!camHandle)
+		{
+			camHandle = openCamera(index);
+			tmpHandle = true;
+		}
+
+		setNodeValue<int64_t>(camHandle, NODE_NAME_OFFSETX, settings.offsetX.value);
+		setNodeValue<int64_t>(camHandle, NODE_NAME_OFFSETY, settings.offsetY.value);
+		setNodeValue<int64_t>(camHandle, NODE_NAME_WIDTH, settings.width.value);
+		setNodeValue<int64_t>(camHandle, NODE_NAME_HEIGHT, settings.height.value);
+		setNodeValue<int64_t>(camHandle, NODE_NAME_GAIN, settings.gain.value);
+		setNodeValue<int64_t>(camHandle, NODE_NAME_PIXELFORMAT, settings.pixelFormat);
+
+		if(tmpHandle)
+			closeCamera(camHandle);
+
+		return true;
+	}
+	catch(JaiException& ex)
+	{
+		return false;
+	}
 }
 
 CAM_HANDLE JaiNodeModule::openCamera(int index)
@@ -112,8 +251,97 @@ int64_t queryNodeValue<int64_t>(CAM_HANDLE hCamera, const char* nodeName)
 	int64_t value;
 	J_STATUS_TYPE retval = J_Camera_GetValueInt64(hCamera, (int8_t*)nodeName, &value);
 	if (retval != J_ST_SUCCESS)
-		throw JaiException(retval);
+		throw JaiException(retval, "Couldn't retrieve Int64 value");
 	return value;
+}
+
+template<typename T> 
+void setNodeValue(CAM_HANDLE hCamera, const char* nodeName, T value) {}
+
+template<>
+void setNodeValue<int64_t>(CAM_HANDLE hCamera, const char* nodeName, int64_t value)
+{
+	J_STATUS_TYPE retval = J_Camera_SetValueInt64(hCamera, (int8_t*)nodeName, value);
+	if (retval != J_ST_SUCCESS)
+		throw JaiException(retval, "Couldn't set Int64 value");
+}
+
+template<typename T>
+RangedValue<T> queryNodeRangedValue(CAM_HANDLE hCamera, const char* nodeName)
+{
+	return RangedValue<T>();
+}
+
+template<>
+RangedValue<int64_t> queryNodeRangedValue<int64_t>(CAM_HANDLE hCamera, const char* nodeName)
+{
+	NODE_HANDLE hNode = nullptr;
+	J_STATUS_TYPE retval;
+	RangedValue<int64_t> rangedValue;
+
+	if((retval = J_Camera_GetNodeByName(hCamera, 
+		(int8_t*) nodeName, &hNode)) == J_ST_SUCCESS)
+	{
+		retval = J_Node_GetMinInt64(hNode, &rangedValue.minValue);
+		retval = J_Node_GetMaxInt64(hNode, &rangedValue.maxValue);
+		retval = J_Node_GetValueInt64(hNode, false, &rangedValue.value);
+	}
+
+	return rangedValue;
+}
+
+vector<tuple<int64_t, string>> queryNodeEnumValue(CAM_HANDLE hCamera, const char* nodeName)
+{
+	NODE_HANDLE hNode = nullptr;
+	J_STATUS_TYPE retval;
+	vector<tuple<int64_t, string>> enumValues;
+
+	if((retval = J_Camera_GetNodeByName(hCamera, 
+		(int8_t*)nodeName, &hNode)) == J_ST_SUCCESS)
+	{
+		uint32_t numEnums;
+		if((retval = J_Node_GetNumOfEnumEntries(hNode,
+			&numEnums)) == J_ST_SUCCESS)
+		{
+			for(uint32_t i = 0; i < numEnums; ++i)
+			{
+				NODE_HANDLE hEnumNode = nullptr;
+				if((retval = J_Node_GetEnumEntryByIndex(hNode,
+					i, &hEnumNode)) == J_ST_SUCCESS)
+				{
+					J_NODE_ACCESSMODE accessMode;
+					if((retval =J_Node_GetAccessMode(hEnumNode,
+						&accessMode)) == J_ST_SUCCESS
+						&& (accessMode != NI)
+						&& (accessMode != NA))
+					{
+						int8_t buffer[J_CONFIG_INFO_SIZE];
+						uint32_t size = sizeof(buffer);
+
+						if((retval = J_Node_GetDescription(hEnumNode,
+							buffer, &size)) != J_ST_SUCCESS)
+						{
+							continue;
+						}
+
+						string desc = reinterpret_cast<char*>(buffer);
+						int64_t value;
+
+						if((retval = J_Node_GetEnumEntryValue(hEnumNode,
+							&value)) != J_ST_SUCCESS)
+						{
+							continue;
+						}
+
+						
+						enumValues.emplace_back(std::tie(value, desc));
+					}
+				}
+			}
+		}
+	}
+
+	return enumValues;
 }
 
 #endif
--- Logic/Jai/JaiNodeModule.h
+++ Logic/Jai/JaiNodeModule.h
@@ -6,12 +6,50 @@
 
 #include <Jai_Factory.h>
 
+using std::vector;
+using std::string;
+using std::tuple;
+
 /// TODO: 
 /// * Support for 10-bit bayer formats
 /// * JaiDynamicFactory
 /// * White-balance RGB gain finder
-/// * Quering pixel format, width, height and adc gain values for properties (LUTSample)
 /// * (LONG) Use lower primitives (StreamThread instead of StreamCallback)
+
+struct CameraInfo
+{
+	string id;
+	string modelName;
+	string manufacturer;
+	string interfaceId;
+	string ipAddress;
+	string macAddress;
+	string serialNumber;
+	string userName;
+};
+
+template<typename T>
+struct RangedValue
+{
+	RangedValue() : minValue(0), maxValue(0), value(0) {}
+	RangedValue(T minValue, T maxValue, T value)
+		: minValue(minValue), maxValue(maxValue), value(value) {}
+
+	T minValue;
+	T maxValue;
+	T value;
+};
+
+struct CameraSettings
+{
+	RangedValue<int64_t> offsetX;
+	RangedValue<int64_t> offsetY;
+	RangedValue<int64_t> width;
+	RangedValue<int64_t> height;
+	RangedValue<int64_t> gain;
+	int64_t pixelFormat;
+	vector<tuple<int64_t, string>> pixelFormats;	
+};
 
 class LOGIC_EXPORT JaiNodeModule : public NodeModule
 {
@@ -22,16 +60,22 @@ public:
 	bool initialize() override;
 	bool ensureInitialized() override;
 
-	std::string moduleName() const override;
+	string moduleName() const override;
+
+	vector<CameraInfo> discoverCameras();
+	CameraSettings cameraSettings(int index);
+	bool setCameraSettings(int index, const CameraSettings& settings);
 
 	CAM_HANDLE openCamera(int index);
 	void closeCamera(CAM_HANDLE camHandle);
 
 private:
 	FACTORY_HANDLE _factoryHandle;
-	std::vector<CAM_HANDLE> _camHandles;
+	vector<CAM_HANDLE> _camHandles;
 };
 	
+#define NODE_NAME_OFFSETX "OffsetX"
+#define NODE_NAME_OFFSETY "OffsetY"
 #define NODE_NAME_WIDTH "Width"
 #define NODE_NAME_HEIGHT "Height"
 #define NODE_NAME_PIXELFORMAT "PixelFormat"
