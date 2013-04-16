f8cbbaa2bbda399d2b62b8be0dacebbe7afe51c8
k0zmo <koozmoo@gmail.com>
2013-04-16 21:31:34 +0200

    added detector&descriptor nodes (BRISK and SURF)
    BRISK is now initialized only on property change

--- Logic/Nodes/BriskNodes.cpp
+++ Logic/Nodes/BriskNodes.cpp
@@ -2,6 +2,7 @@
 #include "NodeFactory.h"
 
 #include <opencv2/features2d/features2d.hpp>
+using std::unique_ptr;
 
 class BriskFeatureDetectorNodeType : public NodeType
 {
@@ -15,20 +16,23 @@ public:
 
 	bool setProperty(PropertyID propId, const QVariant& newValue) override
 	{
+		if(propId > ID_PatternScale || propId < ID_Threshold)
+			return false;
+
 		switch(propId)
 		{
 		case ID_Threshold:
 			_thresh = newValue.toInt();
-			return true;
 		case ID_NumOctaves:
 			_nOctaves = newValue.toInt();
-			return true;
 		case ID_PatternScale:
 			_patternScale  = newValue.toFloat();
-			return true;
 		}
 
-		return false;
+		// That's a bit cheating here - creating BRISK object takes time (approx. 200ms for PAL)
+		// which if done per frame makes it slowish (more than SIFT actually)
+		_brisk = unique_ptr<cv::BRISK>(new cv::BRISK(_thresh, _nOctaves, _patternScale));
+		return true;
 	}
 
 	QVariant property(PropertyID propId) const override
@@ -51,8 +55,7 @@ public:
 		if(src.rows == 0 || src.cols == 0)
 			return ExecutionStatus(EStatus::Ok);
 
-		cv::BRISK detector(_thresh, _nOctaves, _patternScale);
-		detector.detect(src, keypoints);
+		_brisk->detect(src, keypoints);
 
 		return ExecutionStatus(EStatus::Ok);
 	}
@@ -80,7 +83,7 @@ public:
 		nodeConfig.pProperties = prop_config;
 	}
 
-private:
+protected:
 	enum EPropertyID
 	{
 		ID_Threshold,
@@ -91,11 +94,18 @@ private:
 	int _thresh;
 	int _nOctaves;
 	float _patternScale;
+	// Must be pointer since BRISK doesn't implement copy/move operator (they should have)
+	unique_ptr<cv::BRISK> _brisk;
 };
 
 class BriskFeatureExtractorNodeType : public NodeType
 {
 public:
+	BriskFeatureExtractorNodeType()
+		: _brisk(new cv::BRISK())
+	{
+	}
+
 	ExecutionStatus execute(NodeSocketReader& reader, NodeSocketWriter& writer) override
 	{
 		const cv::Mat& imageSrc = reader.readSocket(0).getImage();
@@ -108,9 +118,7 @@ public:
 		cv::KeyPoints& outKeypoints = writer.acquireSocket(1).getKeypoints();
 
 		outKeypoints = keypoints;
-
-		cv::BRISK extractor;
-		extractor.compute(imageSrc, outKeypoints, outDescriptors);
+		_brisk->compute(imageSrc, outKeypoints, outDescriptors);
 
 		return ExecutionStatus(EStatus::Ok);
 	}
@@ -132,7 +140,46 @@ public:
 		nodeConfig.pInputSockets = in_config;
 		nodeConfig.pOutputSockets = out_config;
 	}
+
+private:
+	// Must be pointer since BRISK doesn't implement copy/move operator (they should have)
+	unique_ptr<cv::BRISK> _brisk;
+};
+
+class BriskNodeType : public BriskFeatureDetectorNodeType
+{
+public:
+
+	ExecutionStatus execute(NodeSocketReader& reader, NodeSocketWriter& writer) override
+	{
+		const cv::Mat& src = reader.readSocket(0).getImage();
+		cv::KeyPoints& keypoints = writer.acquireSocket(0).getKeypoints();
+		cv::Mat& descriptors = writer.acquireSocket(1).getArray();
+
+		if(src.rows == 0 || src.cols == 0)
+			return ExecutionStatus(EStatus::Ok);
+
+		//cv::BRISK(_thresh, _nOctaves, _patternScale)
+		(*_brisk)(src, cv::noArray(), keypoints, descriptors);
+
+		return ExecutionStatus(EStatus::Ok);
+	}
+
+	void configuration(NodeConfig& nodeConfig) const override
+	{
+		BriskFeatureDetectorNodeType::configuration(nodeConfig);
+
+		static const OutputSocketConfig out_config[] = {
+			{ ENodeFlowDataType::Keypoints, "keypoints", "Keypoints", "" },
+			{ ENodeFlowDataType::Array, "output", "Descriptors", "" },
+			{ ENodeFlowDataType::Invalid, "", "", "" }
+		};
+
+		nodeConfig.description = "";
+		nodeConfig.pOutputSockets = out_config;
+	}
 };
 
 REGISTER_NODE("Features/BRISK Extractor", BriskFeatureExtractorNodeType)
 REGISTER_NODE("Features/BRISK Detector", BriskFeatureDetectorNodeType)
+REGISTER_NODE("Features/BRISK", BriskNodeType)
