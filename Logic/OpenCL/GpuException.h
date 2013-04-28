218a15dd6c85e4190c0fbf5751f1ee7967a0c39e
k0zmo <koozmoo@gmail.com>
2013-04-29 00:59:16 +0200

    preprocess gpu exception message in its ctor

--- Logic/OpenCL/GpuException.h
+++ Logic/OpenCL/GpuException.h
@@ -5,34 +5,64 @@
 #include "Prerequisites.h"
 
 #include <exception>
+#include <sstream>
 
 struct GpuNodeException : public std::exception
 {
 	GpuNodeException(int error, const std::string& message)
 		: error(error), message(message)
-	{}
+	{
+		formatMessage();
+	}
 
 	virtual const char* what() const noexcept
 	{
-		return "GpuNodeException";
+		return formatted.c_str();
 	}
 
 	int error;
 	std::string message;
+	std::string formatted;
+
+private:
+	void formatMessage()
+	{
+		std::ostringstream strm;
+		strm << "OpenCL error (" << error << "): " << message;
+		formatted = strm.str();
+	}
 };
 
 struct GpuBuildException : public std::exception
 {
 	GpuBuildException(const std::string& log)
 		: log(log)
-	{}
+	{
+		formatMessage();
+	}
 
 	virtual const char* what() const noexcept
 	{
-		return "GpuBuildException";
+		return formatted.c_str();
 	}
 
 	std::string log;
+	std::string formatted;
+
+private:
+	void formatMessage()
+	{
+		std::string logMessage = log;
+		if(logMessage.size() > 1024)
+		{
+			logMessage.erase(1024, std::string::npos);
+			logMessage.append("\n...");
+		}
+
+		std::ostringstream strm;
+		strm << "Building program failed: \n" << logMessage;
+		formatted = strm.str();
+	}
 };
 
 #endif
