46c052b61919163572d4fb54153bad82e91a382d
k0zmo <koozmoo@gmail.com>
2013-05-28 11:00:47 +0200

    added CUDA SURF from OpenCV (for comparison)

--- Logic/Nodes/SurfNodes.cpp
+++ Logic/Nodes/SurfNodes.cpp
@@ -197,6 +197,137 @@ public:
 	}
 };
 
+#include <opencv2/opencv_modules.hpp>
+#ifdef HAVE_OPENCV_GPU
+
+#include <opencv2/gpu/gpu.hpp>
+#include <opencv2/nonfree/gpu.hpp>
+
+class SurfGpuNodeType : public NodeType
+{
+public:
+	SurfGpuNodeType()
+		: _hessianThreshold(400.0)
+		, _nOctaves(4)
+		, _nOctaveLayers(2)
+		, _extended(false)
+		, _upright(false)
+	{
+	}
+
+	bool setProperty(PropertyID propId, const QVariant& newValue) override
+	{
+		switch(propId)
+		{
+		case ID_HessianThreshold:
+			_hessianThreshold = newValue.toDouble();
+			return true;
+		case ID_NumOctaves:
+			_nOctaves = newValue.toInt();
+			return true;
+		case ID_NumOctaveLayers:
+			_nOctaveLayers = newValue.toInt();
+			return true;
+		case ID_Extended:
+			_extended = newValue.toBool();
+			return true;
+		case ID_Upright:
+			_upright = newValue.toBool();
+			return true;
+		}
+
+		return false;
+	}
+
+	QVariant property(PropertyID propId) const override
+	{
+		switch(propId)
+		{
+		case ID_HessianThreshold: return _hessianThreshold;
+		case ID_NumOctaves: return _nOctaves;
+		case ID_NumOctaveLayers: return _nOctaveLayers;
+		case ID_Extended: return _extended;
+		case ID_Upright: return _upright;
+		}
+
+		return QVariant();
+	}
+
+	ExecutionStatus execute(NodeSocketReader& reader, NodeSocketWriter& writer) override
+	{
+		// inputs
+		const cv::Mat& src = reader.readSocket(0).getImage();
+		// outputs
+		KeyPoints& kp = writer.acquireSocket(0).getKeypoints();
+		cv::Mat& descriptors = writer.acquireSocket(1).getArray();
+
+		// validate inputs
+		if(src.empty())
+			return ExecutionStatus(EStatus::Ok);
+
+		cv::gpu::GpuMat img;
+		img.upload(src);
+
+		cv::gpu::SURF_GPU surf;
+		cv::gpu::GpuMat keypointsGPU;
+		cv::gpu::GpuMat descriptorsGPU;
+
+		surf(img, cv::gpu::GpuMat(), keypointsGPU, descriptorsGPU);
+
+		surf.downloadKeypoints(keypointsGPU, kp.kpoints);
+		descriptorsGPU.download(descriptors);
+		kp.image = src;
+
+		return ExecutionStatus(EStatus::Ok, 
+			formatMessage("Keypoints detected: %d", (int) kp.kpoints.size()));
+	}
+
+	void configuration(NodeConfig& nodeConfig) const override
+	{
+		static const InputSocketConfig in_config[] = {
+			{ ENodeFlowDataType::Image, "image", "Image", "" },
+			{ ENodeFlowDataType::Invalid, "", "", "" }
+		};
+		static const OutputSocketConfig out_config[] = {
+			{ ENodeFlowDataType::Keypoints, "keypoints", "Keypoints", "" },
+			{ ENodeFlowDataType::Array, "output", "Descriptors", "" },
+			{ ENodeFlowDataType::Invalid, "", "", "" }
+		};
+		static const PropertyConfig prop_config[] = {
+			{ EPropertyType::Double, "Hessian threshold", "" },
+			{ EPropertyType::Integer, "Number of octaves", "min:1" },
+			{ EPropertyType::Integer, "Number of octave layers", "min:1" },
+			{ EPropertyType::Boolean, "Extended", "" },
+			{ EPropertyType::Boolean, "Upright", "" },
+			{ EPropertyType::Unknown, "", "" }
+		};
+
+		nodeConfig.description = "";
+		nodeConfig.pInputSockets = in_config;
+		nodeConfig.pOutputSockets = out_config;
+		nodeConfig.pProperties = prop_config;
+	}
+
+protected:
+	enum EPropertyID
+	{
+		ID_HessianThreshold,
+		ID_NumOctaves,
+		ID_NumOctaveLayers,
+		ID_Extended,
+		ID_Upright
+	};
+
+	double _hessianThreshold;
+	int _nOctaves;
+	int _nOctaveLayers;
+	bool _extended;
+	bool _upright;
+};
+
+REGISTER_NODE("Features/CUDA SURF", SurfGpuNodeType)
+#endif
+
 REGISTER_NODE("Features/SURF Extractor", SurfFeatureExtractorNodeType)
 REGISTER_NODE("Features/SURF Detector", SurfFeatureDetectorNodeType)
 REGISTER_NODE("Features/SURF", SurfNodeType)
