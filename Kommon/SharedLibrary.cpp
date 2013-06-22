a0feb40cc688d559da905322941bc8e8cb6cb6a2
k0zmo <koozmoo@gmail.com>
2013-06-22 12:44:17 +0200

    added basic shared library handling for both OSes

---
+++ Kommon/SharedLibrary.cpp
@@ -0,0 +1,41 @@
+#include "SharedLibrary.h"
+
+namespace SharedLibrary {
+
+#if K_SYSTEM == K_SYSTEM_WINDOWS
+
+LibraryHandle loadLibrary(const char* path)
+{
+	HMODULE moduleHandle = ::LoadLibraryA(path);
+	if(!moduleHandle)
+		throw std::runtime_error("Could not load given module");
+	return moduleHandle;
+}
+
+void unloadLibrary(LibraryHandle sharedLibraryHandle)
+{
+	BOOL result = ::FreeLibrary(sharedLibraryHandle);
+	if(result == FALSE)
+		throw std::runtime_error("Could not unload given module");
+}
+
+#elif K_SYSTEM == K_SYSTEM_LINUX
+
+LibraryHandle loadLibrary(const char* path)
+{
+	void* sharedObject = ::dlopen(path, RTLD_NOW);
+	if(!sharedObject)
+		throw std::runtime_error("Could not load given shared object");
+	return sharedObject;
+}
+
+void unloadLibrary(LibraryHandle sharedLibraryHandle)
+{
+	int result = ::dlclose(sharedLibraryHandle);
+	if(result != 0)
+		throw std::runtime_error("Could not unload given module");
+}
+
+#endif
+
+}
---
+++ Kommon/SharedLibrary.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "konfig.h"
+#include <stdexcept>
+
+#if K_SYSTEM == K_SYSTEM_WINDOWS
+#  define WIN32_LEAN_AND_MEAN
+#  define VC_EXTRALEAN
+#  include <windows.h>
+typedef HMODULE LibraryHandle;
+#elif K_SYSTEM == K_SYSTEM_LINUX
+#  include <dlfcn.h>
+typedef void* LibraryHandle;
+#else
+#  error "OS not supported"
+#endif
+
+namespace SharedLibrary {
+
+LibraryHandle loadLibrary(const char* path);
+void unloadLibrary(LibraryHandle sharedLibraryHandle);
+
+template<class Signature>
+	Signature* GetFunctionAddress(LibraryHandle sharedLibraryHandle, 
+		const char* functionName)
+	{
+#if K_SYSTEM == K_SYSTEM_WINDOWS
+		FARPROC functionAddress = ::GetProcAddress(sharedLibraryHandle, functionName);
+		if(!functionAddress)
+			throw std::runtime_error("Could not find exported function");
+		return reinterpret_cast<Signature*>(functionAddress);
+#elif K_SYSTEM == K_SYSTEM_LINUX
+		::dlerror(); // clear error value
+
+		void* functionAddress = ::dlsym(sharedLibraryHandle, functionName);
+		const char* error = ::dlerror(); // check for error
+		if(!error)
+			throw std::runtime_error("Could not find exported function");
+		return reinterpret_cast<Signature*>(functionAddress);
+#endif
+	}
+}
