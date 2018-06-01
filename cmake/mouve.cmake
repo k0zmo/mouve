9a8e56fe54cde8f2523d8c39e9210c03256d7255
k0zmo <k0zmo@outlook.com>
2018-06-01 13:09:10 +0200

    Add mouve_add_plugin() function  for adding new plugin target and setting its common properites

--- cmake/mouve.cmake
+++ cmake/mouve.cmake
@@ -1,1 +1,27 @@
-# empty for now
+function(mouve_add_plugin _name)
+    add_library(${_name} MODULE ${ARGN})
+    target_link_libraries(${_name} PRIVATE Mouve.Logic)
+    target_include_directories(${_name} PRIVATE ${mouve_SOURCE_DIR})
+
+    if(MSVC) # We should really check for multi-config generator
+        foreach(config ${CMAKE_CONFIGURATION_TYPES})
+            string(TOUPPER ${config} configUpper)
+            set_target_properties(${_name}
+                PROPERTIES 
+                    LIBRARY_OUTPUT_DIRECTORY_${configUpper} ${CMAKE_BINARY_DIR}/bin/${config}/plugins
+            )
+        endforeach()
+    else()
+        set_target_properties(${_name}
+            PROPERTIES 
+                LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin/plugins
+
+        )
+    endif()
+
+    install(TARGETS ${_name}
+        RUNTIME DESTINATION bin/plugins
+        LIBRARY DESTINATION bin/plugins
+        ARCHIVE DESTINATION lib/plugins
+    )
+endfunction()
