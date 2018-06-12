4ebd07f80899c320e8938c0ab85c39aa1eade2a7
k0zmo <k0zmo@outlook.com>
2018-06-12 20:48:59 +0200

    Fix CMake configure error when no build type is specified

--- cmake/mouve.cmake
+++ cmake/mouve.cmake
@@ -1,11 +1,3 @@
-macro(mouve_bin_directory _config _directory)
-    if(MSVC) # We should really check for multi-config generator
-        set(${_directory} ${CMAKE_BINARY_DIR}/bin/${_config})
-    else()
-        set(${_directory} ${CMAKE_BINARY_DIR}/bin)
-    endif()
-endmacro()
-
 function(mouve_add_kernels _target)
     set(boolean "")
     set(one_value_args SUBDIR)
@@ -48,15 +40,13 @@ function(mouve_add_plugin _name)
     if(MSVC) # We should really check for multi-config generator
         foreach(config ${CMAKE_CONFIGURATION_TYPES})
             string(TOUPPER ${config} configUpper)
-            mouve_bin_directory(${config} directory)
             set_target_properties(${_name} PROPERTIES 
-                LIBRARY_OUTPUT_DIRECTORY_${configUpper} ${directory}/plugins
+                LIBRARY_OUTPUT_DIRECTORY_${configUpper} ${CMAKE_BINARY_DIR}/bin/${config}/plugins
             )
         endforeach()
     else()
-        mouve_bin_directory(${CMAKE_BUILD_TYPE} directory)
         set_target_properties(${_name} PROPERTIES
-            LIBRARY_OUTPUT_DIRECTORY ${directory}/plugins
+            LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin/plugins
         )
     endif()
 
