7f746b00b5e4473587f5bad448780cbfe0c66e12
k0zmo <k0zmo@outlook.com>
2018-07-12 21:42:21 +0200

    Move homography nodes to separate plug-in

--- plugins/features2d-homography/nodes/Homography.cpp
+++ plugins/features2d-homography/nodes/Homography.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014 Kajetan Swierk <k0zmo@outlook.com>
+ * Copyright (c) 2013-2018 Kajetan Swierk <k0zmo@outlook.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -34,8 +34,7 @@ using std::vector;
 class EstimateHomographyNodeType : public NodeType
 {
 public:
-    EstimateHomographyNodeType()
-        : _reprojThreshold(3.0)
+    EstimateHomographyNodeType() : _reprojThreshold(3.0)
     {
         addInput("Matches", ENodeFlowDataType::Matches);
         addOutput("Homography", ENodeFlowDataType::Array);
@@ -55,12 +54,13 @@ public:
         Matches& outMt = writer.acquireSocket(1).getMatches();
 
         // Validate inputs
-        if(mt.queryPoints.empty() || mt.trainPoints.empty()
-        || mt.queryImage.empty() || mt.trainImage.empty())
+        if (mt.queryPoints.empty() || mt.trainPoints.empty() || mt.queryImage.empty() ||
+            mt.trainImage.empty())
             return ExecutionStatus(EStatus::Ok);
 
-        if(mt.queryPoints.size() != mt.trainPoints.size())
-            return ExecutionStatus(EStatus::Error, 
+        if (mt.queryPoints.size() != mt.trainPoints.size())
+            return ExecutionStatus(
+                EStatus::Error,
                 "Points from one images doesn't correspond to key points in another one");
 
         // Do stuff
@@ -70,25 +70,25 @@ public:
         outMt.trainPoints.clear();
         size_t kpSize = mt.queryPoints.size();
 
-        if(mt.queryPoints.size() < 4)
+        if (mt.queryPoints.size() < 4)
         {
             H = cv::Mat::ones(3, 3, CV_64F);
             return ExecutionStatus(EStatus::Ok, "Not enough matches for homography estimation");
         }
 
         vector<uchar> inliersMask;
-        cv::Mat homography = cv::findHomography(mt.queryPoints, mt.trainPoints, 
-            CV_RANSAC, _reprojThreshold, inliersMask);
-        int inliersCount = (int) std::count(begin(inliersMask), end(inliersMask), 1);
-        if(inliersCount < 4)
+        cv::Mat homography = cv::findHomography(mt.queryPoints, mt.trainPoints, CV_RANSAC,
+                                                _reprojThreshold, inliersMask);
+        int inliersCount = (int)std::count(begin(inliersMask), end(inliersMask), 1);
+        if (inliersCount < 4)
             return ExecutionStatus(EStatus::Ok);
 
         vector<cv::Point2f> queryPoints(inliersCount), trainPoints(inliersCount);
 
         // Create vector of inliers only
-        for(size_t inlier = 0, idx = 0; inlier < kpSize; ++inlier)
+        for (size_t inlier = 0, idx = 0; inlier < kpSize; ++inlier)
         {
-            if(inliersMask[inlier] == 0)
+            if (inliersMask[inlier] == 0)
                 continue;
 
             queryPoints[idx] = mt.queryPoints[inlier];
@@ -133,9 +133,7 @@ private:
 class KnownHomographyInliersNodeType : public NodeType
 {
 public:
-    KnownHomographyInliersNodeType()
-        : _homographyPath(Filepath{})
-        , _reprojThreshold(3.0)
+    KnownHomographyInliersNodeType() : _homographyPath(Filepath{}), _reprojThreshold(3.0)
     {
         addInput("Matches", ENodeFlowDataType::Matches);
         addOutput("Homography", ENodeFlowDataType::Array);
@@ -156,13 +154,14 @@ public:
         Matches& outMt = writer.acquireSocket(1).getMatches();
 
         // Validate inputs
-        if(mt.queryPoints.empty() || mt.trainPoints.empty()
-        || mt.queryImage.empty() || mt.trainImage.empty())
+        if (mt.queryPoints.empty() || mt.trainPoints.empty() || mt.queryImage.empty() ||
+            mt.trainImage.empty())
             return ExecutionStatus(EStatus::Ok);
 
-        if(mt.queryPoints.size() != mt.trainPoints.size())
-            return ExecutionStatus(EStatus::Error, 
-            "Points from one images doesn't correspond to key points in another one");
+        if (mt.queryPoints.size() != mt.trainPoints.size())
+            return ExecutionStatus(
+                EStatus::Error,
+                "Points from one images doesn't correspond to key points in another one");
 
         // Do stuff - Load real homography
         std::ifstream homographyFile(_homographyPath.cast<Filepath>().data(), std::ios::in);
@@ -174,18 +173,27 @@ public:
         }
 
         cv::Mat homography(3, 3, CV_32F);
-        homographyFile >> homography.at<float>(0, 0) >> homography.at<float>(0, 1) >> homography.at<float>(0, 2);
-        homographyFile >> homography.at<float>(1, 0) >> homography.at<float>(1, 1) >> homography.at<float>(1, 2);
-        homographyFile >> homography.at<float>(2, 0) >> homography.at<float>(2, 1) >> homography.at<float>(2, 2);
+        homographyFile >> homography.at<float>(0, 0) >> homography.at<float>(0, 1) >>
+            homography.at<float>(0, 2);
+        homographyFile >> homography.at<float>(1, 0) >> homography.at<float>(1, 1) >>
+            homography.at<float>(1, 2);
+        homographyFile >> homography.at<float>(2, 0) >> homography.at<float>(2, 1) >>
+            homography.at<float>(2, 2);
 
         // Normalize if h[2][2] isnt close to 1.0 and 0.0
-        float h22 = homography.at<float>(2,2);
-        if(std::fabs(h22) > 1e-5 && std::fabs(h22 - 1.0) > 1e-5)
+        float h22 = homography.at<float>(2, 2);
+        if (std::fabs(h22) > 1e-5 && std::fabs(h22 - 1.0) > 1e-5)
         {
             h22 = 1.0f / h22;
-            homography.at<float>(0,0) *= h22; homography.at<float>(0,1) *= h22; homography.at<float>(0,2) *= h22;
-            homography.at<float>(1,0) *= h22; homography.at<float>(1,1) *= h22; homography.at<float>(1,2) *= h22;
-            homography.at<float>(2,0) *= h22; homography.at<float>(2,1) *= h22; homography.at<float>(2,2) *= h22;
+            homography.at<float>(0, 0) *= h22;
+            homography.at<float>(0, 1) *= h22;
+            homography.at<float>(0, 2) *= h22;
+            homography.at<float>(1, 0) *= h22;
+            homography.at<float>(1, 1) *= h22;
+            homography.at<float>(1, 2) *= h22;
+            homography.at<float>(2, 0) *= h22;
+            homography.at<float>(2, 1) *= h22;
+            homography.at<float>(2, 2) *= h22;
         }
 
         outMt.queryImage = mt.queryImage;
@@ -193,20 +201,25 @@ public:
         outMt.queryPoints.clear();
         outMt.trainPoints.clear();
 
-        for(size_t i = 0; i < mt.queryPoints.size(); ++i)
+        for (size_t i = 0; i < mt.queryPoints.size(); ++i)
         {
             const auto& pt1 = mt.queryPoints[i];
             const auto& pt2 = mt.trainPoints[i];
 
-            float invW = 1.0f / (homography.at<float>(2,0)*pt1.x + homography.at<float>(2,1)*pt1.y + homography.at<float>(2,2));
-            float xt = (homography.at<float>(0,0)*pt1.x + homography.at<float>(0,1)*pt1.y + homography.at<float>(0,2)) * invW;
-            float yt = (homography.at<float>(1,0)*pt1.x + homography.at<float>(1,1)*pt1.y + homography.at<float>(1,2)) * invW;
-            
+            float invW = 1.0f / (homography.at<float>(2, 0) * pt1.x +
+                                 homography.at<float>(2, 1) * pt1.y + homography.at<float>(2, 2));
+            float xt = (homography.at<float>(0, 0) * pt1.x + homography.at<float>(0, 1) * pt1.y +
+                        homography.at<float>(0, 2)) *
+                       invW;
+            float yt = (homography.at<float>(1, 0) * pt1.x + homography.at<float>(1, 1) * pt1.y +
+                        homography.at<float>(1, 2)) *
+                       invW;
+
             float xdist = xt - pt2.x;
             float ydist = yt - pt2.y;
-            float dist = std::sqrt(xdist*xdist + ydist*ydist);
+            float dist = std::sqrt(xdist * xdist + ydist * ydist);
 
-            if(dist <= _reprojThreshold)
+            if (dist <= _reprojThreshold)
             {
                 outMt.queryPoints.push_back(pt1);
                 outMt.trainPoints.push_back(pt2);
